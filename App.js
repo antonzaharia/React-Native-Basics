cb8f1b2494bb97bf1365929b531660cc298623d6
antonzaharia <zaharia_anton@yahoo.com>
2020-12-12 16:27:44 +0000

    first page

--- App.js
+++ App.js
@@ -1,43 +1,17 @@
 import React from "react";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
-import {
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  Alert,
-  Button,
-  View,
-  Dimentions,
-} from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { StyleSheet } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   const { landscape } = useDeviceOrientation();
 
-  const handlePress = () =>
-    Alert.alert("My title", "My Message", [
-      { text: "Yes", onPress: () => console.log("Yes") },
-      { text: "No", onPress: () => console.log("No") },
-    ]);
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.div}></View>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "orange",
     justifyContent: "center",
     alignItems: "center",
-  },
-  div: {
-    backgroundColor: "dodgerblue",
-    width: "50%",
-    height: landscape ? "100%" : "30%",
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "red",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
