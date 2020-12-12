269dd5485ba101572e42ea24f808626401345834
antonzaharia <zaharia_anton@yahoo.com>
2020-12-12 16:37:24 +0000

    logo container added

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
 
 function WelcomeScreen(props) {
   return (
@@ -7,6 +7,10 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style>Sell What You Don't Need</Text>
+      </View>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -16,6 +20,16 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 100,
   },
   loginButton: {
     width: "100%",
